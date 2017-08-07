5602d4e9b91cabd374eebe608126e826adcb719b
Dan MacTough <danmactough@gmail.com>
2017-08-07 18:18:34 -0400

    Demonstrate the problem

---
+++ test/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "mocha": true
+    },
+    // "globals": {
+    //     "_": true,
+    //     "assert": true,
+    //     "debug": true,
+    //     "knexClient": true,
+    //     "mockDb": true,
+    //     "should": true,
+    //     "supertest": true,
+    //     "tracker": true
+    // },
+    "rules": {
+        "node/no-unpublished-require": [
+            0
+        ]
+    }
+};
