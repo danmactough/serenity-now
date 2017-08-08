20974a12c03eb5a6fd7e00a917b075d1d3c49d4d
Dan MacTough <danmactough@gmail.com>
2017-08-08 12:59:01 -0400

    Explicitly use the domain attached to the bluebird promise

--- on-unhandled-rejection.js
+++ on-unhandled-rejection.js
@@ -1,5 +1,13 @@
 module.exports = function onUnhandledRejection (reason, p) {
   const error = reason instanceof Error ? reason : new Error(reason);
   console.error('Unhandled Rejection at:', p, '\nreason:', error); // eslint-disable-line no-console
-  throw error;
+  const currentDomain = p.domain;
+  if (currentDomain) {
+    currentDomain.run(function () {
+      throw error;
+    });
+  }
+  else {
+    throw error;
+  }
 };
